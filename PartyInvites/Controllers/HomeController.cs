862ffd10a7e56ff56dff1a16b0b1b0c9e33974d7
Volodymyr <volodymyr482@gmail.com>
2017-09-05 23:20:35 +0300

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброго ранку" : "Доброго дня";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestRespons guest)
+        {
+      if(ModelState.IsValid)
+            return View("Thanks", guest);
+            else
+            return View();
+            
+        }
+    }
+
+}
---
+++ PartyInvites/Models/GuestRespons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestRespons
+    {
+        [Required(ErrorMessage ="Будь-ласка, введіть своє ім'я")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Будь-ласка, введіть email ")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Ви ввелт некоректний Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Будь-ласка, введіть телефон")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Будь-ласка. вкажіть, будети ви на вечірці")]
+        public bool? WillAttend { get; set; }
+    }
+}
